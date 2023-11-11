e7a39860e06afcaf85788fc7c9c5690860670194
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-11-11 09:50:50 -0500

    estilos al seleccionar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,20 +22,33 @@ class _HomePageState extends State<HomePage> {
                 TextField(
                   decoration: InputDecoration(
                     hintText: "Ingrese el nombre de la institución",
-                    label: Text("Nombre"),
-                    icon: Icon(Icons.people),
-                    prefixIcon: Icon(Icons.share),
-                    suffix: Icon(Icons.abc),
-                    filled: true,
-                    fillColor: Color.fromARGB(255, 255, 23, 2),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50)),
+
+                    //ESTILO MÁS GENERICO
+                    // label: Text("Nombre"),
+                    // icon: Icon(Icons.people),
+                    // prefixIcon: Icon(Icons.share),
+                    // suffix: Icon(Icons.abc),
+                    // filled: true,
+                    // fillColor: Color.fromARGB(255, 255, 23, 2),
+
+                    //EL ESTILO QUE TOMA AL INICIO SIN QUE SE SELECCIONE EL TEXTFIELD
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 5,
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    //ESTILO QUE TOMA CUANDO SELECCIONO EL TEXTFIELD
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                        width: 10,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
-                // IconButton(
-                //   onPressed: () {},
-                //   icon: Icon(Icons.access_time_rounded),
-                // ),
                 SizedBox(
                   height: 50,
                 ),
