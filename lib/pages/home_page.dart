7816f92dc497750be3b75a782f5eb8b62523857c
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-11-11 09:41:49 -0500

    style text field

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,13 +22,30 @@ class _HomePageState extends State<HomePage> {
                 TextField(
                   decoration: InputDecoration(
                     hintText: "Ingrese el nombre de la institución",
-                    // label: Text("Nombre"),
+                    label: Text("Nombre"),
+                    icon: Icon(Icons.people),
+                    prefixIcon: Icon(Icons.share),
+                    suffix: Icon(Icons.abc),
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 255, 23, 2),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50)),
                   ),
+                ),
+                // IconButton(
+                //   onPressed: () {},
+                //   icon: Icon(Icons.access_time_rounded),
+                // ),
+                SizedBox(
+                  height: 50,
                 ),
                 TextField(
                   decoration: InputDecoration(
                     hintText: "Ingrese la dirección de la institución",
                     label: Text("Dirección"),
+                    icon: Icon(Icons.map),
+                    filled: true,
+                    fillColor: Colors.yellow,
                   ),
                 ),
               ],
