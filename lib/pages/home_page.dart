5554e9457e87e3c4cbaa2835905b7307fbb95f27
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-11-10 23:03:50 -0500

    first properties textfield

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,32 +6,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int notas = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text("Home Page aquiiiiii"),
+          title: Text("Home Page"),
         ),
         body: Padding(
           padding: EdgeInsets.all(8),
           child: SingleChildScrollView(
             child: Column(
               children: [
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Ingrese el nombre de la institución",
+                    // label: Text("Nombre"),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Ingrese la dirección de la institución",
+                    label: Text("Dirección"),
+                  ),
+                ),
               ],
             ),
           ),
