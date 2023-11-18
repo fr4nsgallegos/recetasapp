4f6ed8ff54c3549867aec8fb95b0c35fbd6fca02
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-11-18 12:58:37 -0500

    boton de retroceder

--- lib/pages/recipe_page.dart
+++ lib/pages/recipe_page.dart
@@ -10,6 +10,15 @@ class RecipePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        //MOSTRAR AUTOMATICAMENTE EL BOTON DE RETRONCESO
+        automaticallyImplyLeading: false,
+        //IMPLEMENTAR UN BOTON PERSONALIZADA EN EL LEADING (PUEDE SER UN BOTON PERSONALIZADO PARA HACER NAVIGATOR POP)
+        leading: IconButton(
+          icon: Icon(Icons.ac_unit_outlined),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         title: Text(title),
       ),
       body: Column(
